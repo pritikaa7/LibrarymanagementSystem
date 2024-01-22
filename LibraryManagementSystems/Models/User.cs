ce5cdb4875135f25599573e06e802444518fd9d3
nepal10 <12201902@students.koi.edu.au>
2024-01-22 11:41:42 +1100

    new changes

---
+++ LibraryManagementSystems/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystems.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+
+        public string  userName { get; set; }
+        public string Password { get; set; }
+    }
+}
